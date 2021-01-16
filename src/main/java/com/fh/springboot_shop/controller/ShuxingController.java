bc44ce20073dfcbc7f2d5f652bad4b3492d1096e
lx <857116797@qq.com>
2021-01-16 17:36:33 +0800

    同源策略

--- src/main/java/com/fh/springboot_shop/controller/ShuxingController.java
+++ src/main/java/com/fh/springboot_shop/controller/ShuxingController.java
@@ -5,14 +5,12 @@ import com.fh.springboot_shop.entity.vo.ResultData;
 import com.fh.springboot_shop.entity.vo.ShuxingParam;
 import com.fh.springboot_shop.service.ShuxingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
+@CrossOrigin
 @RequestMapping("api/shuxing")
 public class ShuxingController {
 
